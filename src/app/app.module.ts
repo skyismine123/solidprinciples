ac952ae3235c6395d44c7d918510fce98496c3fb
mzivanovic <milan.zivanovic@adition.com>
2021-10-28 12:04:54 +0200

    adding widget component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import { SocialNetworkWidgetComponent } from './social-network-widget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SocialNetworkWidgetComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/social-network-widget.component.ts
@@ -0,0 +1,137 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'iv-social-network-widget',
+  template: `
+    <div *ngIf="type === 'facebook'">
+      <header> Facebook </header>
+      <div class="mini-stat clearfix bg-facebook rounded">
+        <span class="mini-stat-icon"><i class="fa fa-facebook fg-facebook"></i></span>
+        <div class="mini-stat-info">
+          <span>5,762</span>
+        </div>
+      </div>
+    </div>
+    <div *ngIf="type === 'twitter'">
+      <header> Twitter </header>
+      <div class="mini-stat clearfix bg-twitter rounded">
+        <span class="mini-stat-icon"><i class="fa fa-twitter fg-twitter"></i></span>
+        <div class="mini-stat-info">
+          <span>7,153</span>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [
+    `/* Social network widget */
+    body{
+      background:#eee;
+      margin-top:20px;
+    }
+
+    .rounded {
+      -webkit-border-radius: 3px !important;
+      -moz-border-radius: 3px !important;
+      border-radius: 3px !important;
+    }
+
+    .mini-stat {
+      padding: 15px;
+      margin-bottom: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .mini-stat-icon {
+      width: 60px;
+      height: 60px;
+      display: inline-block;
+      line-height: 60px;
+      text-align: center;
+      font-size: 30px;
+      background: none repeat scroll 0% 0% #EEE;
+      border-radius: 100%;
+      float: left;
+      margin-right: 10px;
+      color: #FFF;
+      cursor: pointer;
+    }
+
+    .mini-stat-info {
+      font-size: 12px;
+      padding-top: 2px;
+    }
+
+    span, p {
+      color: white;
+    }
+
+    .mini-stat-info span {
+      display: block;
+      font-size: 30px;
+      font-weight: 600;
+      margin-bottom: 5px;
+      margin-top: 7px;
+    }
+
+    /* ================ colors =====================*/
+    .bg-facebook {
+      background-color: #3b5998 !important;
+      border: 1px solid #3b5998;
+      color: white;
+    }
+
+    .fg-facebook {
+      color: #3b5998 !important;
+    }
+
+    .bg-twitter {
+      background-color: #00a0d1 !important;
+      border: 1px solid #00a0d1;
+      color: white;
+    }
+
+    .fg-twitter {
+      color: #00a0d1 !important;
+    }
+
+    .bg-googleplus {
+      background-color: #db4a39 !important;
+      border: 1px solid #db4a39;
+      color: white;
+    }
+
+    .fg-googleplus {
+      color: #db4a39 !important;
+    }
+
+    .bg-bitbucket {
+      background-color: #205081 !important;
+      border: 1px solid #205081;
+      color: white;
+    }
+
+    .fg-bitbucket {
+      color: #205081 !important;
+    }
+
+    header {
+      color: #1976d2;
+      border: 3px dotted #1976d2;
+      background: transparent;
+      padding: 10px;
+    }
+    `
+  ]
+})
+export class SocialNetworkWidgetComponent implements OnInit {
+
+  @Input() type: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
